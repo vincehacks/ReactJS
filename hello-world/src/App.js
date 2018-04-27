96661abdee2a435141f1fb40242bb54f0843bdb0
Vince Chang <vincechang@Vinces-MacBook-Pro.local>
2018-04-27 08:08:52 -0700

    Added hello world project

---
+++ hello-world/src/App.js
@@ -0,0 +1,9 @@
+// Importing the react library from the npm module
+import React from 'react';
+
+const App = () => {
+  return (<div className="App">Hello World!</div>);
+};
+
+
+export default App;
