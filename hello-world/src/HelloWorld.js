f3ae1135793c57a8dad64926f9f1ad28bf5f65ef
Vince Chang <vincechang@Vinces-MacBook-Pro.local>
2018-04-29 09:41:52 -0700

    Added state function that will change the greeting from English to Chinese

--- hello-world/src/HelloWorld.js
+++ hello-world/src/HelloWorld.js
@@ -1,10 +1,45 @@
 // Importing the react library from the npm module
-import React from 'react';
+import React, {Component} from 'react';
 import './HelloWorld.css';
 
-const HelloWorld = (props) => {
-  return (<div className="HelloWorld">Hello {props.name}!</div>);
-};
+// const HelloWorld = (props) => {
+//   return (<div className="HelloWorld">Hello {props.name}!</div>);
+// };
+
+
+class HelloWorld extends Component {
+
+  // Constructor
+  constructor(props){
+    super(props);
+    this.state = {greeting: "Hello "};
+    // This tells JavaScript that any time I see 'this' in chineseify, I want
+    // you to refer to ME!
+    this.chineseify = this.chineseify.bind(this);
+  }
+
+  // Method that handles the onClick
+  chineseify(){
+    // You can't modify the state inside a class, so need to use this.setState
+    // to do it for me. ex. can't do greeting = "Ni hao"
+    this.setState({greeting: "Ni Hao "});
+  }
+
+
+  render(){
+    return (
+      // this.props.name will come from only this current class, not parent's
+      <div className='HelloWorld'>
+        {this.state.greeting} {this.props.name}!
+        <br/>
+        <button onClick={this.chineseify}>Chineseify!</button>
+      </div>
+    );
+  }
+}
+
+
+
 
 // Export this component so it could be used in other files!
 export default HelloWorld;
