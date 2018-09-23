cd2a3e92df2c187e72534b11b812052d47d26734
vincehacks <vincechangsf@gmail.com>
2018-09-22 19:44:25 -0700

    Trying a push from webstorm

--- github_users/src/App.js
+++ github_users/src/App.js
@@ -5,34 +5,34 @@ import Form from './Form';
 
 class App extends Component  {
 
-    // Actual data that is initially being rendered
-    state = {
-        cards: [
-            {
-                name:"Vince Chang",
-                avatar_url:"https://avatars2.githubusercontent.com/u/11703494?v=4",
-                login:"vincehacks"
-            },
-            {
-                name:"Zach Ho",
-                avatar_url:"https://avatars1.githubusercontent.com/u/15718708?v=4",
-                login:"zachho123"
-            }
-        ]
-    };
+  // Actual data that is initially being rendered
+  state = {
+    cards: [
+      {
+        name: "Vince Chang",
+        avatar_url: "https://avatars2.githubusercontent.com/u/11703494?v=4",
+        login: "vincehacks"
+      },
+      {
+        name: "Zach Ho",
+        avatar_url: "https://avatars1.githubusercontent.com/u/15718708?v=4",
+        login: "zachho123"
+      }
+    ]
+  };
 
-    addNewCard = (cardInfo) => {
-        this.setState({cards: this.state.cards.concat(cardInfo)});
-    };
+  addNewCard = (cardInfo) => {
+    this.setState({cards: this.state.cards.concat(cardInfo)});
+  };
 
-    render(){
-        return (
-            <div className="App">
-                <Form onSubmit={this.addNewCard}/>
-                <CardList cards={this.state.cards}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Form onSubmit={this.addNewCard}/>
+        <CardList cards={this.state.cards}/>
+      </div>
+    );
+  }
 }
 
 export default App;
