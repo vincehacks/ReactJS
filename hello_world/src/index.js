15aeb121317471a95dad330a23b7c5cbee3a32b1
Vince Chang <vincechangsf@gmail.com>
2019-09-26 07:50:24 -0700

    Adding npm modules so I can run the app locally again

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -4,8 +4,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 // Render takes 2 parameters (which component to render, and where to render)
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
